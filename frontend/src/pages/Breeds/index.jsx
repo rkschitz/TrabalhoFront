d7e6e86066a9228553424a977a6138d3603c222f
Renan <renanalbino.dias@gmail.com>
2024-12-02 19:04:54 -0300

    docker final

--- frontend/src/pages/Breeds/index.jsx
+++ frontend/src/pages/Breeds/index.jsx
@@ -3,16 +3,21 @@ import { getBreeds, alimentBreed } from "../../api/breed";
 import { favoriteBreed } from "../../api/userBreed";
 import { AuthContext } from '../../auth/Context'
 import { toast } from 'react-toastify'
+import BreedModal from '../../components/BreedForm/index';
+import Button from 'react-bootstrap/Button';
 import './styles.css';
 
 export default function BreedFeed() {
-    const [breeds, setBreeds] = useState([]); // Inicializa breeds como array vazio
+    const [breeds, setBreeds] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+    const [isUpdate, setIsUpdate] = useState(false);
+    const [currentBreed, setCurrentBreed] = useState(null);
     const { id, role } = useContext(AuthContext);
 
     const getBreed = async () => {
         try {
             const response = await getBreeds();
-            setBreeds(response || []); // Garante que breeds seja um array
+            setBreeds(response || []);
         } catch (error) {
             console.error("Erro ao buscar as raças:", error);
         }
@@ -21,7 +26,7 @@ export default function BreedFeed() {
     const favorite = (breedId) => async () => {
         try {
             const response = await favoriteBreed(id, breedId, false);
-            if (response.status === 201){
+            if (response.status === 201) {
                 toast('Raça favoritada com sucesso');
             }
         } catch (error) {
@@ -39,21 +44,38 @@ export default function BreedFeed() {
         }
     };
 
+    const handleEdit = (breed) => {
+        setCurrentBreed(breed);
+        setIsUpdate(true);
+        setShowModal(true);
+    }
+
     useEffect(() => {
         getBreed();
-    }, []);
+    }, [isUpdate]);
 
     return (
         <div className="breed-feed">
             <h1>Raças</h1>
-            {role === 'admin' && <button onClick={() => alimetFeed()}>Alimentar Feed</button>}
+            {role === 'admin' && (<><button onClick={() => alimetFeed()}>Alimentar Feed</button>
+                <Button variant="primary" onClick={() => { setCurrentBreed(null); setIsUpdate(false); setShowModal(true); }}>
+                    Adicionar Nova Raça
+                </Button>
+            </>)}
             {breeds.map((breed) => (
                 <div key={breed.breedId} className="breed">
-                    <button className="favorite-button" onClick={favorite(breed.breedId, true)}>Favoritar❤️</button>
+                    <button className="favorite-button" onClick={favorite(breed.breedId, true)}>Favoritar</button>
+                    {role === 'admin' && <button className="favorite-button" onClick={() => handleEdit(breed)}>Alterar</button>}
                     <p>ID: {breed.breedId}</p>
                     <img src={breed.image} alt={`Imagem da raça ${breed.breedId}`} className="breed-image" />
                 </div>
             ))}
+            <BreedModal
+                show={showModal}
+                handleClose={() => setShowModal(false)}
+                setIsUpdate={setIsUpdate}
+                breed={currentBreed}
+            />
         </div>
     );
 }
